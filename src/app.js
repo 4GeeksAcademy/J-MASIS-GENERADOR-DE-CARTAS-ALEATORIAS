c383e43b0aef69a3a90e78c7bbe93aaaaeead018
juanMasis777 <juanmasisg@gmail.com>
2026-03-07 06:06:41 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  window.onload = function () {
+    console.log("Hello Rigo from the console!");
+
+    import "bootstrap";
+    import "./style.css";
+
+    window.onload = function () {
+
+      console.log("Hello Rigo from the console!");
+
+      let card = document.getElementById("card");
+      let top = document.querySelector(".top");
+      let bottom = document.querySelector(".bottom");
+      let number = document.querySelector(".number");
+
+      let palos = ["♦", "♥", "♠", "♣"];
+      let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+      function createCard() {
+
+        let azarPalo = Math.floor(Math.random() * palos.length);
+        let azarNumber = Math.floor(Math.random() * numbers.length);
+
+        top.innerHTML = palos[azarPalo];
+        bottom.innerHTML = palos[azarPalo];
+        number.innerHTML = numbers[azarNumber];
+
+        if (palos[azarPalo] == "♦" || palos[azarPalo] == "♥") {
+          card.style.color = "red";
+        } else {
+          card.style.color = "black";
+        }
+
+      }
+
+      createCard();
+
+    };
+
+    if (h) {
+      card.style.height = h + "px";
+    }
+
+  });
+
 };
